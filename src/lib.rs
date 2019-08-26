607e2dc09e00026451c4df789128bec7d8553c89
galoz <gal.oz@powtoon.com>
2019-08-26 14:49:35 +0300

    some initial untested functions better algo

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
+use image::{self,
+            imageops::blur,
+            png::PNGReader::Read,
+            GenericImage,
+            ImageDecoder};
+use exoquant::*;
+
+
+const ASCII_CHARS: [char;7] = ['8', '%', '=', '+', '@', 'W', 'X'];
+
 mod pixle_analysis {
 
-    const ASCII_CHARS: (char) = ('#', ' . ', )
 
-/// kernel based  RGB color averaging for each cell:
-/// different shades of blue would need to be in the same character (done by analysis of color distance in LAB space:
-/// https://www.pyimagesearch.com/2016/02/15/determining-object-color-with-opencv/)
-/// OR:
-/// different shades of the same color will be the same color - meaning that the image will be compressed into ~20 colors accordingly -
+/// kernel based RGB color averaging for each cell:
+/// different shades of the same color will be the same character
 /// with each color having a unique character assignment (character assignment is unique on each run - as it's more fun) (~20 as per number of colors)
+///
+/// We will build a highly quantized image for the character analysis, and use the-original-but-blurred image for character coloration.
 
 /// combine both to a cell struct along with a locator(x, y)
 /// such that we will have a cell with character + color + location, read to be written
@@ -16,20 +24,32 @@ mod pixle_analysis {
 }
 
 
-mod image_handling {
+/// image_handling:
 
+// import image: either .png or .jpg -> with GenericImageView trat
+// I think we can do it with O:read image::png::PNGDecoder:new(raw_image)?
 
-    use image::imageops::blur;
-    use image::png::PNGReader::Read;
+/// process image (gaussian blur)
+fn initial_image_processing(path: &str) -> impl GenericImage {
 
-    // import image: either .png or .jpg -> with GenericImageView trat
-    // I think we can do it with O:read image::png::PNGDecoder:new(raw_image)?
+    let img = image::open(path).unwrap();
+    // get side and see that it fits with kernel:
+    let dim = img.dimensions();
+    let w = dim.0 - (dim.0%9); // TODO: replace by kernel value...
+    let h = dim.1 - (dim.1%9); // TODO: replace by kernel value...
+    cropped_img = img.thumbnail(w, h);
+    drop(img);
+    cropped_img
 
-    // get dimensions and crop to fit pixel-kernel-size - image.dimensions()...
-
-
-    /// process image (gaussian blur)
-    /// each mapped to a character (assignment will be semi-random as it's more fun!)
-
-    /// save ascii to .png image (other formats maybe too?)
 }
+
+fn quantized_image(&image: impl GenericImage, num_colors: usize) -> (Vec<Color>, Vec<u8>) {
+    let (palette, indexed_data) = convert_to_indexed(
+        image.pixels, image.width, num_colors,
+      &optimizer::KMeans, &ditherer::FloydSteinberg::new());
+    return (palette, indexed_data)
+}
+/// each mapped to a character (assignment will be semi-random as it's more fun!)
+
+/// save ascii to .png image (other formats maybe too?)
+
