a3110d68e29d58a5a1df6103ea56825f81033f1b
galoz <gal.oz@powtoon.com>
2019-08-26 13:37:18 +0300

    first outline

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,35 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod pixle_analysis {
+
+    const ASCII_CHARS: (char) = ('#', ' . ', )
+
+/// kernel based  RGB color averaging for each cell:
+/// different shades of blue would need to be in the same character (done by analysis of color distance in LAB space:
+/// https://www.pyimagesearch.com/2016/02/15/determining-object-color-with-opencv/)
+/// OR:
+/// different shades of the same color will be the same color - meaning that the image will be compressed into ~20 colors accordingly -
+/// with each color having a unique character assignment (character assignment is unique on each run - as it's more fun) (~20 as per number of colors)
+
+/// combine both to a cell struct along with a locator(x, y)
+/// such that we will have a cell with character + color + location, read to be written
+/// into the terminal
+
+}
+
+
+mod image_handling {
+
+
+    use image::imageops::blur;
+    use image::png::PNGReader::Read;
+
+    // import image: either .png or .jpg -> with GenericImageView trat
+    // I think we can do it with O:read image::png::PNGDecoder:new(raw_image)?
+
+    // get dimensions and crop to fit pixel-kernel-size - image.dimensions()...
+
+
+    /// process image (gaussian blur)
+    /// each mapped to a character (assignment will be semi-random as it's more fun!)
+
+    /// save ascii to .png image (other formats maybe too?)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
 
+    const KERNEL9: [i8; 3] = [3, 3, 3];
+    const KERNEL25: [i8; 5] = [5, 5, 5, 5, 5];
+
+ // get image and args from environment (terminal)
+
+ // spawn threads to handle pixle analysis and produce terminal cells
+
+ // write to screen at the correct sequence and\or save to file
 }
