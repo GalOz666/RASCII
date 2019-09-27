e0a72eb4faa0f78552500fc6495deecad15e576d
GalOz666 <yavinarba@gmail.com>
2019-09-27 17:09:11 +0300

    Feature/more functional (#5)
    * performance boost
    
    * hashmap still broken but cache algo is fine
    remove to_luma to main
    
    * working with cache - still not optimal with memory
    
    * cache and speed optimized somewhat =p

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,29 @@
 #![allow(non_snake_case)]
 
 use std::env::args;
-use std::io;
-use std::io::Write;
 
 use ansi_term::Colour::RGB;
 
-use RASCII::{initial_image_processing, structs};
+use rascii::{initial_image_processing, structs};
 
-const ASCII_CHARS: [char;11] = [':', '8', '%', '=', ',', '@', '.', 'X', '&', '~', 'S'];
+static ASCII_CHARS: [char;11] = [':', '8', '%', '=', ',', '@', '.', 'X', '&', '~', 'S'];
 
 fn main() {
     let args: Vec<String> = args().collect();
-    let kernel = structs::Kernel::new(9);
+    let mut kernel = structs::Kernel::new(9);
     assert!(args.len() as usize >= 2, "file path was not provided!");
     let (img, (width, height)) = initial_image_processing(&args[1], &kernel);
     let mut current_point = [0, 0];
-    let stdout = io::stdout();
-    let mut lock = stdout.lock();
+    let grey = img.to_luma();
     while current_point[1] < height {
-
-        let cell = kernel.to_char_cell(&current_point, &img, &ASCII_CHARS);
-
+        let cell = kernel.to_char_cell(&current_point, &img, &grey, &ASCII_CHARS);
         let (r, g, b) = cell.color;
         let ascii  = RGB(r, g, b).paint(cell.ascii.to_string());
         current_point = if current_point[0] + kernel.kernel() < width {
-            write!(lock, "{} ", ascii).unwrap();
+            print!("{} ", ascii);
             [current_point[0] + kernel.kernel(), current_point[1]]
         } else {
-            writeln!(lock, "{} ", ascii).unwrap();
+            println!("{} ", ascii);
             [0, current_point[1] + (kernel.kernel())]
         };
     }
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,31 +1,35 @@
+use std::collections::HashMap;
+
 use counter::Counter;
-use image::{self, DynamicImage, GenericImageView, Luma, Rgba};
+use image::{self, DynamicImage, GenericImageView, GrayImage, Luma, Rgba};
 
 use crate::grey_to_ascii;
 
 pub struct CharCell {
-    pub x: usize,
-    pub y: usize,
+
     pub color: (u8, u8, u8),
-    pub ascii: char
+    pub ascii: char,
+
 }
 
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+#[derive(Debug)]
 pub struct Kernel {
 
     kernel: u32,
+    _cache: HashMap<u8, char>,
+
 }
 
 impl Kernel {
     pub fn new(num: u32) -> Self {
-        Kernel { kernel: num }
+        Kernel { kernel: num, _cache: HashMap::new() }
     }
     pub fn kernel(&self) -> u32 {
         self.kernel
     }
 
     fn get_kernel_locators(&self, start_pos: &[u32; 2]) -> Vec<[u32; 2]> {
-        let mut kern: Vec<[u32; 2]> = Vec::new();
+        let mut kern: Vec<[u32; 2]> = Vec::with_capacity(self.kernel as usize);
         let num = (self.kernel() as f64).sqrt() as u32;
         assert_eq!(num as f64, (self.kernel() as f64).sqrt(),
         "kernel needs to have a non float square root!");
@@ -51,10 +55,9 @@ impl Kernel {
         }
         colors
     }
-    fn kernel_greys(&self, kernel_locator: &Vec<[u32; 2]>, image: &DynamicImage) -> Vec<u8> {
+    fn kernel_greys(&self, kernel_locator: &Vec<[u32; 2]>, image: &GrayImage) -> Vec<u8> {
         assert_eq!(kernel_locator.len() as u32, self.kernel(),
         "kernel locator lenght and actual kernel value are not the same!");
-        let image = image.to_luma();
         let mut colors = Vec::with_capacity(self.kernel() as usize);
 
         for loc in kernel_locator {
@@ -73,26 +76,31 @@ impl Kernel {
         let f = counter.most_common_ordered();
         f[0].0.to_vec()
     }
-    fn dominant_grey_by_kernel(&self, kernel_locator: &Vec<[u32; 2]>, image: &DynamicImage) -> u8 {
-        let colors: Vec<u8> = self.kernel_greys(kernel_locator, image);
+    fn dominant_grey_by_kernel(&self, kernel_locator: &Vec<[u32; 2]>, grey: &GrayImage) -> u8 {
+        let colors: Vec<u8> = self.kernel_greys(kernel_locator, grey);
         let counter: Counter<_, u8> = colors.iter().collect();
         let f = counter.most_common_ordered();
         *f[0].0
     }
 
-    pub fn to_char_cell(&self, start_pos: &[u32; 2], image:  &DynamicImage, ascii: &[char]) -> CharCell {
+    pub fn to_char_cell(&mut self, start_pos: &[u32; 2], image:  &DynamicImage, grey: &GrayImage, ascii_list: &'static [char]) -> CharCell {
 
         let x  = start_pos[0] as f64 / self.kernel() as f64;
         let y = start_pos[1] as f64 / self.kernel() as f64;
         assert_eq!(x.trunc(), x, "x start position does not conform to kernel size");
         assert_eq!(y.trunc(), y, "y start position does not conform to kernel size");
-        let x = x as usize;
-        let y = y as usize;
         let locators = self.get_kernel_locators(start_pos);
         let color = self.dominant_color_by_kernel(&locators, image);
-        let grey_color = self.dominant_grey_by_kernel(&locators, image);
-        let ascii = grey_to_ascii(grey_color, ascii);
-
-        return CharCell { x, y , color: (color[0], color[1], color[2]), ascii }
+        let grey_color = self.dominant_grey_by_kernel(&locators, grey);
+        // caching unknown / fetching known results:
+        let ascii = match self._cache.get(&grey_color) {
+            Some(asci) => *asci,
+            None => {
+                let a = grey_to_ascii(grey_color, ascii_list);
+                self._cache.insert(grey_color, a);
+                self._cache.get(&grey_color).unwrap().clone()
+            }
+        };
+        return CharCell { color: (color[0], color[1], color[2]), ascii }
     }
 }
