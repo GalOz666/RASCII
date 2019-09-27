5c4412e2481a8166098f97821a13fe120ccfd5bc
galoz <gal.oz@powtoon.com>
2019-09-27 12:04:50 +0300

    hashmap still broken but cache algo is fine remove to_luma to main

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,27 @@ use std::io::Write;
 
 use ansi_term::Colour::RGB;
 
-use RASCII::{initial_image_processing, structs};
+use rascii::{initial_image_processing, structs};
 
-const ASCII_CHARS: [char;11] = [':', '8', '%', '=', ',', '@', '.', 'X', '&', '~', 'S'];
+static ASCII_CHARS: [char;11] = [':', '8', '%', '=', ',', '@', '.', 'X', '&', '~', 'S'];
 
 fn main() {
     let args: Vec<String> = args().collect();
-    let kernel = structs::Kernel::new(9);
+    let mut kernel = structs::Kernel::new(9);
     assert!(args.len() as usize >= 2, "file path was not provided!");
     let (img, (width, height)) = initial_image_processing(&args[1], &kernel);
     let mut current_point = [0, 0];
     let grey = img.to_luma();
-    let stdout = io::stdout();
-    let mut lock = stdout.lock();
+    let mut line = Vec::with_capacity((width/kernel.kernel()) as usize);
     while current_point[1] < height {
-
         let cell = kernel.to_char_cell(&current_point, &img, &grey, &ASCII_CHARS);
-
         let (r, g, b) = cell.color;
         let ascii  = RGB(r, g, b).paint(cell.ascii.to_string());
         current_point = if current_point[0] + kernel.kernel() < width {
-            write!(lock, "{} ", ascii).unwrap();
+            print!(lock, "{} ", ascii).unwrap();
             [current_point[0] + kernel.kernel(), current_point[1]]
         } else {
-            writeln!(lock, "{} ", ascii).unwrap();
+            println!("{} ", line).unwrap();
             [0, current_point[1] + (kernel.kernel())]
         };
     }
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,31 +1,36 @@
+use std::cmp::Ord;
+use std::collections::HashMap;
+
 use counter::Counter;
 use image::{self, DynamicImage, GenericImageView, GrayImage, Luma, Rgba};
 
 use crate::grey_to_ascii;
 
-pub struct CharCell {
-    pub x: usize,
-    pub y: usize,
+pub struct CharCell<'q> {
+
     pub color: (u8, u8, u8),
-    pub ascii: char
+    pub ascii: &'q char,
+
 }
 
 #[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
 pub struct Kernel {
 
     kernel: u32,
+    _cache: HashMap<u8, &'static char>,
+
 }
 
 impl Kernel {
     pub fn new(num: u32) -> Self {
-        Kernel { kernel: num }
+        Kernel { kernel: num, _cache: HashMap::new() }
     }
     pub fn kernel(&self) -> u32 {
         self.kernel
     }
 
     fn get_kernel_locators(&self, start_pos: &[u32; 2]) -> Vec<[u32; 2]> {
-        let mut kern: Vec<[u32; 2]> = Vec::new();
+        let mut kern: Vec<[u32; 2]> = Vec::with_capacity(self.kernel as usize);
         let num = (self.kernel() as f64).sqrt() as u32;
         assert_eq!(num as f64, (self.kernel() as f64).sqrt(),
         "kernel needs to have a non float square root!");
@@ -79,19 +84,24 @@ impl Kernel {
         *f[0].0
     }
 
-    pub fn to_char_cell(&self, start_pos: &[u32; 2], image:  &DynamicImage, grey: &GrayImage, ascii: &[char]) -> CharCell {
+    pub fn to_char_cell(&mut self, start_pos: &[u32; 2], image:  &DynamicImage, grey: &GrayImage, ascii_list: &'static [char]) -> CharCell {
 
         let x  = start_pos[0] as f64 / self.kernel() as f64;
         let y = start_pos[1] as f64 / self.kernel() as f64;
         assert_eq!(x.trunc(), x, "x start position does not conform to kernel size");
         assert_eq!(y.trunc(), y, "y start position does not conform to kernel size");
-        let x = x as usize;
-        let y = y as usize;
         let locators = self.get_kernel_locators(start_pos);
         let color = self.dominant_color_by_kernel(&locators, image);
         let grey_color = self.dominant_grey_by_kernel(&locators, grey);
-        let ascii = grey_to_ascii(grey_color, ascii);
-
-        return CharCell { x, y , color: (color[0], color[1], color[2]), ascii }
+        // caching unknown / fetching known results:
+        let ascii = match self._cache.get(&grey_color) {
+            Some(asci) => *asci,
+            None => {
+                let a = grey_to_ascii(grey_color, ascii_list);
+                self._cache[&grey_color] = a;
+                a
+            }
+        };
+        return CharCell { color: (color[0], color[1], color[2]), ascii }
     }
 }
