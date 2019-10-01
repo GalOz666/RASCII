62ab12675209769fc611c9002d40eaf3922f4547
galoz <gal.oz@powtoon.com>
2019-10-01 13:30:55 +0300

    small screen option avoids padding bad kernel argument failbacks on 9

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,19 @@ use rascii::{initial_image_processing, structs};
 const DEFAULT_KERN: u32 =  9;
 static ASCII_CHARS: [char;11] = [':', '8', '%', '=', ',', '@', '.', 'X', '&', '~', 'S'];
 
+
 fn main() {
-    // todo: screen resolution optimization (add " " to normal screen - "" to small screen)
     let args: Vec<String> = args().collect();
+    let screen_arg = String::from("--small-screen");
     assert!(args.len() as usize >= 2, "file path was not provided!");
     let kern_arg: u32 = match args.get(2){
-        Some(num) => num.parse().expect("invalid kernel number!"),
+        Some(num) => num.parse().unwrap_or_else(|_x| DEFAULT_KERN),
         None => DEFAULT_KERN
+    };
+    let pad = if args.contains(&screen_arg) {
+        ""
+    } else {
+        " "
     };
     let mut kernel = structs::Kernel::new(kern_arg);
     let (img, (width, height)) = initial_image_processing(&args[1], &kernel);
@@ -26,10 +32,10 @@ fn main() {
         let (r, g, b) = cell.color;
         let ascii  = RGB(r, g, b).paint(cell.ascii.to_string());
         current_point = if current_point[0] + kernel.kernel() < width {
-            print!("{} ", ascii);
+            print!("{}{}", ascii, pad);
             [current_point[0] + kernel.kernel(), current_point[1]]
         } else {
-            println!("{} ", ascii);
+            println!("{}{}", ascii, pad);
             [0, current_point[1] + (kernel.kernel())]
         };
     }
