172af5a8523ca366ab203968fc7322ee6022a432
GalOz666 <yavinarba@gmail.com>
2019-10-01 13:37:07 +0300

    Feature/screenoptimize (#6)
    * performance boost
    
    * hashmap still broken but cache algo is fine
    remove to_luma to main
    
    * working with cache - still not optimal with memory
    
    * cache and speed optimized somewhat =p
    
    * cache with references
    
    * kernel size option added
    
    * small screen option avoids padding
    bad kernel argument failbacks on 9

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,24 @@ use ansi_term::Colour::RGB;
 
 use rascii::{initial_image_processing, structs};
 
+const DEFAULT_KERN: u32 =  9;
 static ASCII_CHARS: [char;11] = [':', '8', '%', '=', ',', '@', '.', 'X', '&', '~', 'S'];
+
 
 fn main() {
     let args: Vec<String> = args().collect();
-    let mut kernel = structs::Kernel::new(9);
+    let screen_arg = String::from("--small-screen");
     assert!(args.len() as usize >= 2, "file path was not provided!");
+    let kern_arg: u32 = match args.get(2){
+        Some(num) => num.parse().unwrap_or_else(|_x| DEFAULT_KERN),
+        None => DEFAULT_KERN
+    };
+    let pad = if args.contains(&screen_arg) {
+        ""
+    } else {
+        " "
+    };
+    let mut kernel = structs::Kernel::new(kern_arg);
     let (img, (width, height)) = initial_image_processing(&args[1], &kernel);
     let mut current_point = [0, 0];
     let grey = img.to_luma();
@@ -20,10 +32,10 @@ fn main() {
         let (r, g, b) = cell.color;
         let ascii  = RGB(r, g, b).paint(cell.ascii.to_string());
         current_point = if current_point[0] + kernel.kernel() < width {
-            print!("{} ", ascii);
+            print!("{}{}", ascii, pad);
             [current_point[0] + kernel.kernel(), current_point[1]]
         } else {
-            println!("{} ", ascii);
+            println!("{}{}", ascii, pad);
             [0, current_point[1] + (kernel.kernel())]
         };
     }
